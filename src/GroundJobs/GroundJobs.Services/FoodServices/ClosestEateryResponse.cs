8ade5e6fe9ba7d873c7b23d857b9e3fa5c643c45
Omid Mufeed <omid@omidmufeed.com>
2015-08-22 19:48:44 +0100

    Merge remote-tracking branch 'origin/master'
    Added SetDistance to the ClosestEateryResponse

--- src/GroundJobs/GroundJobs.Services/FoodServices/ClosestEateryResponse.cs
+++ src/GroundJobs/GroundJobs.Services/FoodServices/ClosestEateryResponse.cs
@@ -1,3 +1,4 @@
+using System;
 using GroundJobs.ServiceBus;
 
 namespace GroundJobs.Services.FoodServices
@@ -7,7 +8,26 @@ namespace GroundJobs.Services.FoodServices
         public PostCodeSearchCommand Command { get; private set; }
         public float Distance;
         public string LocationName;
-        public string Latitude;
-        public string Longitude;
+        public float Latitude;
+        public float Longitude;
+
+        public void SetDistanceFrom(float latitude, float longitude)
+        {
+            var EarthRadius = 6371; // Radius of the earth in km
+            var dLat = DegreeToRadius(latitude - Latitude);
+            var dLon = DegreeToRadius(longitude - Longitude);
+            var a =
+                Math.Sin(dLat/2)*Math.Sin(dLat/2) +
+                Math.Cos(DegreeToRadius(Latitude))*Math.Cos(DegreeToRadius(latitude))*
+                Math.Sin(dLon/2)*Math.Sin(dLon/2)
+                ;
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            Distance = (float)(EarthRadius * c); // Distance in km
+        }
+
+        private double DegreeToRadius(float degree)
+        {
+            return degree*(Math.PI/180);
+        }
     }
 }
--- src/GroundJobs/GroundJobs.Services/FoodServices/GooglePlacesService.cs
+++ src/GroundJobs/GroundJobs.Services/FoodServices/GooglePlacesService.cs
@@ -9,21 +9,22 @@
             postcodeData.Wait();
             var postcode = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(postcodeData.Result);
 
-            var latitude = postcode.result.latitude.ToString();
-            var longitude = postcode.result.longitude.ToString();
+            float latitude = float.Parse(postcode.result.latitude.ToString());
+            float longitude = float.Parse(postcode.result.longitude.ToString());
             var storesData = GetHTMLString($"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius=500&types=food&key=AIzaSyCM_pHgWShJwu4sYj_M79lDQ6Tpw9zV_9k");
             storesData.Wait();
             var googlePlaces = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(storesData.Result);
 
-            if (googlePlaces.results.Count > 0)
-                return new ClosestEateryResponse
-                {
-                    LocationName = googlePlaces.results[0].name.ToString(),
-                    Latitude = googlePlaces.results[0].geometry.location.lat.ToString(),
-                    Longitude = googlePlaces.results[0].geometry.location.lng.ToString(),
-                    Distance = float.Parse("123")
-                };
-            return new ClosestEateryResponse();
+            if (googlePlaces.results.Count <= 0) return new ClosestEateryResponse();
+
+            var closestEatery = new ClosestEateryResponse
+            {
+                LocationName = googlePlaces.results[0].name.ToString(),
+                Latitude = float.Parse(googlePlaces.results[0].geometry.location.lat.ToString()),
+                Longitude = float.Parse(googlePlaces.results[0].geometry.location.lng.ToString()),
+            };
+            closestEatery.SetDistanceFrom(latitude, longitude);
+            return closestEatery;
         }
     }
 }
