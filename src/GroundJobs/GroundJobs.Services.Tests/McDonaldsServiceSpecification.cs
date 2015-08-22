1deb74baafbf81981c52898a0923a7a06f12b1e1
TheAllSeeingPie <joeswan@me.com>
2015-08-22 22:02:15 +0100

    Added unit tests for mcdonalds

---
+++ src/GroundJobs/GroundJobs.Services.Tests/McDonaldsServiceSpecification.cs
@@ -0,0 +1,17 @@
+﻿using GroundJobs.Services.FoodServices;
+using Xunit;
+
+namespace GroundJobs.Services.Tests
+{
+    public class McDonaldsServiceSpecification
+    {
+        [Fact]
+        public void ShouldFindTheLightForLS73NU()
+        {
+            var service = new McDonaldsService();
+            var response = service.Execute(new ClosestEateryRequest { Command = new PostCodeSearchCommand { Postcode = "LS73NU" } });
+            Assert.Equal("McDonald's - Leeds 2", response.LocationName);
+            Assert.Equal(2.95023632F, response.Distance);
+        }
+    }
+}
