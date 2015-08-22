1df8c2abd918b53074a40b53b67e7505ae348bf6
Macs Dickinson <macs.dickinson@hotmail.co.uk>
2015-08-22 17:12:55 +0100

    Added a page for users to enter their job preferences

--- src/GroundJobs/src/GroundJobs.MVC/Controllers/SurveyController.cs
+++ src/GroundJobs/src/GroundJobs.MVC/Controllers/SurveyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNet.Mvc;
+﻿using GroundJobs.MVC.Models;
+using Microsoft.AspNet.Mvc;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -7,9 +8,15 @@ namespace GroundJobs.MVC.Controllers
     public class SurveyController : Controller
     {
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(SurveyIndexViewModel model)
+        {
+            return View(model);
         }
     }
 }
---
+++ src/GroundJobs/src/GroundJobs.MVC/Models/SurveyViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GroundJobs.MVC.Models
+{
+    public class SurveyIndexViewModel
+    {
+        public string Postcode { get; set; }
+        public string WorkLocation { get; set; }
+        public string FavoriteCoffee { get; set; }
+        public string FavoriteBeer { get; set; }
+        public TattooLevel Tattoos { get; set; }
+        public string ArtistsDiscovered { get; set; }
+        public string Diet { get; set; }
+        public string TakeOut { get; set; }
+    }
+
+    public enum TattooLevel
+    {
+        Some = 1,
+        HalfSleeve = 2,
+        FullSleeve = 3,
+        Covered = 4
+    }
+}
