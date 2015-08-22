08a290f21c0a889f7e01cc956bfc6b437616d0aa
TheAllSeeingPie <joeswan@me.com>
2015-08-22 13:49:26 +0100

    Added ServiceBus and unit tests

---
+++ src/GroundJobs/GroundJobs.ServiceBus.Tests/ServiceBusPublishingSpecification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace GroundJobs.ServiceBus.Tests
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ServiceBusPublishingSpecification
+    {
+        
+        public ServiceBusPublishingSpecification()
+        {
+        }
+
+        [Fact]
+        public void PublishShouldAcceptAnICommand()
+        {
+            Assert.Throws<NotImplementedException>(() => ServiceBus.Publish(new TestCommand()));
+        }
+    }
+
+    public class TestCommand : ICommand<TestSubject>
+    {
+        public void Execute(TestSubject data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class TestSubject
+    {
+    }
+}
---
+++ src/GroundJobs/GroundJobs.ServiceBus/ServiceBus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GroundJobs.ServiceBus
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ServiceBus
+    {
+        public static readonly ServiceBus Instance = new ServiceBus();
+
+        public static void Publish<T>(ICommand<T> command)
+        {
+            CommandComplete(Instance, new CommandEventArgs { Command = command});
+        }
+
+        public delegate void OnCommandComplete(CommandEventArgs e);
+        public static event EventHandler<CommandEventArgs> CommandComplete;
+    }
+
+    public class CommandEventArgs : EventArgs
+    {
+        public object Command;
+    }
+
+    public interface ICommand<in T>
+    {
+        void Execute(T data);
+    }
+}
