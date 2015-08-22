4466ca1d4844f7b40de2f7c8007159958fb1d446
TheAllSeeingPie <joeswan@me.com>
2015-08-22 20:06:11 +0100

    Converted servicebus to 4.6 and made an API.
    vNext MVC can suck my balls!

---
+++ src/GroundJobs/GroundJobs.API/Controllers/FoodController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using GroundJobs.Services;
+using GroundJobs.Services.FoodServices;
+
+namespace GroundJobs.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class FoodController : ApiController
+    {
+        // GET api/food/ls73nu
+        [Route("api/food/{postcode}")]
+        public EateryResult Get(string postcode)
+        {
+            return ServiceBus.ServiceBus.Instance.Aggregate<ClosestEateryRequest, ClosestEateryResponse, EateryResult>(new ClosestEateryRequest {Command = new PostCodeSearchCommand {Postcode = postcode}});
+        }
+    }
+}
---
+++ src/GroundJobs/GroundJobs.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GroundJobs.API.WebApiApplication" Language="C#" %>
